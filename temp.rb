1a3fbefc73849ebe4e591c0a0bad58915d7fc2fc
OHASHI Hideya <ohachige@gmail.com>
2013-02-03 00:31:22 +0900

    refactoring

--- temp.rb
+++ temp.rb
@@ -64,11 +64,27 @@ def encode(ary)
   encoded.join
 end
 
+ROMA = {
+:ka => "か",
+:e => "え",
+:ru => "る",
+:no => "の",
+:u => "う",
+:ta => "た",
+:ga => "が",
+:ki => "き",
+:ko => "こ",
+:te => "て",
+:ku => "く",
+:yo => "よ",
+}
+
 def note(vel, symbol, pitchp, tempo, len, len2, offset, lenreq, fixlen,
     endblank, vol, mod, pitchb2, env)
   tempin = "tempin"
   inwav = "#{tempin}.wav"
   inwavfrq = "#{tempin}_wav.frq"
+  symbol = ROMA[symbol]
   FileUtils.ln("#{$oto}/#{symbol}.wav", inwav)
   FileUtils.ln("#{$oto}/#{symbol}_wav.frq", inwavfrq)
   pitchb2 = encode(pitchb2)
@@ -119,22 +135,28 @@ en = [
 [0, 5, 35, 0, 100, 100, 0, 39],
 ]
 
-tempo = 120
-note(100, "か", "C4", tempo, 480, 69.0, 27.0, 600, 81.0, 46.0, 100, 0, pb[0], en[0])
-note(100, "え", "D4", tempo, 480, 28.0, 30.0, 600, 59.0, 48.0, 96, 0, pb[1], en[1])
-note(100, "る", "E4", tempo, 480, 42.0, 23.0, 600, 104.0, 56.0, 100, 0, pb[2], en[2])
-note(100, "の", "F4", tempo, 480, 79.0, 19.0, 650, 77.0, 65.0, 100, 0, pb[3], en[3])
-note(100, "う", "E4", tempo, 480, 6.0, 23.0, 550, 52.0, 37.0, 96, 0, pb[4], en[4])
-note(100, "た", "D4", tempo, 480, 20.0, 29.0, 550, 67.0, 91.0, 100, 0, pb[5], en[5])
-note(100, "が", "C4", tempo, 480, 42.0, 24.0, 600, 96.0, 47.0, 100, 0, pb[6], en[6])
-rest(tempo, 480, -62.0)
-note(100, "き", "E4", tempo, 480, 14.0, 15.0, 550, 108.0, 41.0, 100, 0, pb[7], en[7])
-note(100, "こ", "F4", tempo, 480, 73.0, 22.0, 600, 86.0, 98.0, 100, 0, pb[8], en[8])
-note(100, "え", "G4", tempo, 480, 16.0, 30.0, 550, 59.0, 48.0, 96, 0, pb[9], en[9])
-note(100, "て", "A4", tempo, 480, 28.0, 22.0, 550, 62.0, 65.0, 100, 0, pb[10], en[10])
-note(100, "く", "G4", tempo, 480, 12.0, 26.0, 550, 87.0, 53.0, 100, 0, pb[11], en[11])
-note(100, "る", "F4", tempo, 480, 16.0, 23.0, 550, 104.0, 56.0, 100, 0, pb[12], en[12])
-note(100, "よ", "E4", tempo, 480, 71.0, 5.0, 600, 282.0, 31.0, 100, 0, pb[13], en[13])
+tm = 120
+n4 = tm * 4
+n8 = n4 / 2
+ve = 100
+vo = 100
+m = 0
+
+note(ve, :ka, "C4", tm, n4, 69.0, 27.0, 600,  81.0, 46.0, vo, m, pb[0], en[0])
+note(ve, :e,  "D4", tm, n4, 28.0, 30.0, 600,  59.0, 48.0, 96, m, pb[1], en[1])
+note(ve, :ru, "E4", tm, n4, 42.0, 23.0, 600, 104.0, 56.0, vo, m, pb[2], en[2])
+note(ve, :no, "F4", tm, n4, 79.0, 19.0, 650,  77.0, 65.0, vo, m, pb[3], en[3])
+note(ve, :u,  "E4", tm, n4,  6.0, 23.0, 550,  52.0, 37.0, 96, m, pb[4], en[4])
+note(ve, :ta, "D4", tm, n4, 20.0, 29.0, 550,  67.0, 91.0, vo, m, pb[5], en[5])
+note(ve, :ga, "C4", tm, n4, 42.0, 24.0, 600,  96.0, 47.0, vo, m, pb[6], en[6])
+rest(tm, n4, -62.0)
+note(ve, :ki, "E4", tm, n4, 14.0, 15.0, 550, 108.0, 41.0, vo, m, pb[7], en[7])
+note(ve, :ko, "F4", tm, n4, 73.0, 22.0, 600,  86.0, 98.0, vo, m, pb[8], en[8])
+note(ve, :e,  "G4", tm, n4, 16.0, 30.0, 550,  59.0, 48.0, 96, m, pb[9], en[9])
+note(ve, :te, "A4", tm, n4, 28.0, 22.0, 550,  62.0, 65.0, vo, m, pb[10], en[10])
+note(ve, :ku, "G4", tm, n4, 12.0, 26.0, 550,  87.0, 53.0, vo, m, pb[11], en[11])
+note(ve, :ru, "F4", tm, n4, 16.0, 23.0, 550, 104.0, 56.0, vo, m, pb[12], en[12])
+note(ve, :yo, "E4", tm, n4, 71.0,  5.0, 600, 282.0, 31.0, vo, m, pb[13], en[13])
 
 FileUtils.rm_f($tempwav)
 
